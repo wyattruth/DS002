3f3c891c15f70a5375d1a0b7681228ffbe284d52
wyattruth <94639382+wyattruth@users.noreply.github.com>
2022-03-02 10:25:41 -0800

    Update linear_algebra.py

--- linear_algebra.py
+++ linear_algebra.py
@@ -71,6 +71,41 @@ def distance(v: Vector, w: Vector) -> float:
 def distance(v: Vector, w: Vector) -> float:  # type: ignore
     return magnitude(subtract(v, w))
 
+from typing import Tuple
+
+Matrix = List[List[float]]
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    """Returns (# of rows of A, # of columns of A)"""
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0   # number of elements in first row
+    return num_rows, num_cols
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """Returns the i-th row of A (as a Vector)"""
+    return A[i]
+
+def get_column(A: Matrix, j: int) -> Vector:
+    """Returns the j-th column of A (as a Vector)"""
+    return [A_i[j]          # jth element of row A_i
+            for A_i in A]
+
+from typing import Callable
+
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    Returns a num_rows x num_cols matrix
+    whose (i,j)-th entry is entry_fn(i, j)
+    """
+    return [[entry_fn(i, j)             # given i, create a list
+             for j in range(num_cols)]  #   [entry_fn(i, 0), ... ]
+            for i in range(num_rows)]   # create one list for each i
+
+def identity_matrix(n: int) -> Matrix:
+    """Returns the n x n identity matrix"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
 
 assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
 assert subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
